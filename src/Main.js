d8ecba6bafb685fcf5c6ded271498c753e49edf1
kototo522 <huyejiben@gmail.com>
2020-12-16 09:58:19 +0000

    CSS3DObjectのドラッグ操作の対応

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,6 @@
 import * as THREE from "three/src/Three";
 import { CSS3DObject, CSS3DRenderer } from "three-css3drenderer";
+import { TrackballControls } from "three/examples/jsm/controls/TrackballControls";
 import React from "react";
 import Humberger from "./components/Humberger";
 
@@ -9,102 +10,6 @@ let string =
   '<span class="large">Hello Three.js cookbook</span>' +
   "<textarea> And this is a textarea</textarea>" +
   "</div>";
-
-// global variables
-let control;
-
-class ThreeScene extends React.Component {
-  componentDidMount() {
-    const width = this.mount.clientWidth;
-    const height = this.mount.clientHeight;
-
-    // オブジェクト、カメラ、ライトなどの全ての要素を保持するシーンを作成する
-    this.scene = new THREE.Scene();
-    // カメラの定義 (視野角, アスペクト比, 近くの限界距離, 遠くの限界距離)
-    this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-
-    // 描画領域としてのDOMを設定
-    this.renderer = new CSS3DRenderer();
-    this.renderer.setSize(width, height);
-    this.renderer.domElement.style.position = "absolute";
-    this.renderer.domElement.style.top = 0;
-
-    // カメラを指定座標に設置し、向きをシーンの中央に向ける
-    this.camera.position.x = 500;
-    this.camera.position.y = 475;
-    this.camera.position.z = 767;
-    this.camera.lookAt(this.scene.position);
-
-    // レンダラーの出力をhtml要素に追加する
-    this.mount.appendChild(this.renderer.domElement);
-
-    for (let i = 0; i < 8; i++) {
-      let cssElement = createCSS3DObject(string);
-      cssElement.position.set(100, 100, 100 + 50 * i);
-
-      cssElement.element.addEventListener(
-        "mouseover",
-        (e) => {
-          console.log(e.currentTarget);
-          e.currentTarget.style.top = "200px";
-        },
-        false
-      );
-
-      cssElement.element.addEventListener(
-        "mouseout",
-        (e) => {
-          console.log(e.currentTarget);
-          e.currentTarget.style.top = "0px";
-        },
-        false
-      );
-
-      this.scene.add(cssElement);
-    }
-
-    control = {
-      cameraX: 500,
-      cameraY: 450,
-      cameraZ: 780,
-    };
-
-    this.start();
-  }
-  componentWillUnmount() {
-    this.stop();
-    this.mount.removeChild(this.renderer.domElement);
-  }
-  start = () => {
-    if (!this.frameId) {
-      this.frameId = requestAnimationFrame(this.animate);
-    }
-  };
-  stop = () => {
-    cancelAnimationFrame(this.frameId);
-  };
-  animate = () => {
-    this.renderScene();
-    this.frameId = window.requestAnimationFrame(this.animate);
-  };
-  renderScene = () => {
-    this.renderer.render(this.scene, this.camera);
-  };
-
-  render() {
-    return (
-      <>
-        <div
-          style={{ width: "800px", height: "800px" }}
-          ref={(mount) => {
-            this.mount = mount;
-          }}
-        />
-        <Humberger />
-      </>
-    );
-  }
-}
 
 function createCSS3DObject(s) {
   // 文字列をDOM要素に変換
