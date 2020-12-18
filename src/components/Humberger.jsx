a0c37de2658ce204e45b08cdb348a335a10c8eb3
era1014 <haoirria@gmai.com>
2020-12-19 01:28:53 +0900

    ハンバーガーメニューの調整

--- src/components/Humberger.jsx
+++ src/components/Humberger.jsx
@@ -2,20 +2,37 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 const HumbergerList = styled.ul`
-  margin-top: 150px;
+  margin-top: 50%;
 `;
 
 const HumbergerContent = styled.li`
   font-size: 30px;
-  line-height: 30px;
+  line-height: 70%;
   color: white;
-  margin-top: 50px;
+  margin-top: 30%;
   list-style: none;
-  border-bottom: solid white 1px;
+  border-bottom: solid white 2px;
   padding: 5px;
-  &::before {
-    content: "@";
+  &:nth-child(1)::before {
+    content:"1";
     margin: 0 3px;
+  }
+  &:nth-child(2)::before{
+    content:"2";
+    margin: 0 3px;
+  }
+  &:nth-child(3)::before{
+    content:"3";
+    margin: 0 3px;
+  }
+  &:nth-child(4)::before{
+    content:"4";
+    margin: 0 3px;
+  }
+  &:nth-child(4){
+    position:absolute;
+    bottom: 100px;
+    padding-right:100%;
   }
 `;
 
@@ -23,11 +40,11 @@ const HumbergerBar = styled.div`
   position: fixed;
   top: 0;
   right: ${(props) => (props.isOpen ? "0" : "-320px")};
-  width: 300px;
+  width: 50%;
   height: 100vh;
   background: linear-gradient(
     90deg,
-    rgba(39, 39, 39, 0.8),
+    rgba(39, 39, 39, 0),
     rgba(41, 152, 204, 0.5),
     rgba(51, 190, 255, 0.6)
   );
@@ -36,7 +53,7 @@ const HumbergerBar = styled.div`
 
 const HumbergerButton = styled.div`
   position: fixed;
-  top: 48px;
+  top: 5%;
   right: 72px;
   z-index: 5;
   background-color: white;
@@ -48,9 +65,10 @@ const LineTop = styled.div`
   content: "";
 
   transition: 0.3s;
-  width: 48px;
-  height: 10px;
+  width: 40px;
+  height: 1px;
   padding: 2px;
+  border-radius:20%;
   transition: 0.5s;
   background-color: white;
   transform: rotate(${(props) => (props.isOpen ? 45 : 0)}deg);
@@ -62,9 +80,10 @@ const LineMiddle = styled.div`
   content: "";
 
   transition: 0.5s;
-  width: 48px;
-  height: 10px;
+  width: 40px;
+  height: 1px;
   padding: 2px;
+  border-radius:20%;
   transition: 0.3s all;
   visibility: ${(props) => (props.isOpen ? "hidden" : "visible")};
   background-color: rgba(
@@ -78,9 +97,10 @@ const LineBottom = styled.div`
   content: "";
 
   transition: 0.3s;
-  width: 48px;
-  height: 10px;
+  width: 40px;
+  height: 1px;
   padding: 2px;
+  border-radius:20%;
   transition: 0.5s;
   background-color: white;
   transform: rotate(${(props) => (props.isOpen ? -45 : 0)}deg);
@@ -101,8 +121,7 @@ const Humberger = () => {
           <HumbergerContent>test2</HumbergerContent>
           <HumbergerContent>test3</HumbergerContent>
           <HumbergerContent>test4</HumbergerContent>
-          <HumbergerContent>test5</HumbergerContent>
-          <HumbergerContent>test6</HumbergerContent>
+
         </HumbergerList>
       </HumbergerBar>
     </>
